daaa20f81789b813cd85bfbaab260d775e476bee
Minna <minna1025@gmail.com>
2022-02-10 00:02:03 +0900

    chore: exercise level1 complete

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,28 @@
+// Write a single line comment which says, comments can make code readable
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// Declare four variables without assigning values
+// Declare four variables with assigned values
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/* comments can make code readable, easy to reuse and informative */
+let a, b, c, d;
+const first = 1,
+  second = 2,
+  third = 3,
+  fourth = 4;
+const myInfo = "nayoung \n female \n single \n Korea \n 32";
+const myInfo2 = "nayoung female single Korea 32";
+const myAge = 32;
+const yourAge = 24;
+
+console.log(myInfo);
+console.log(myInfo2);
+console.log(`I am ${myAge} years old.`);
+console.log(`you are ${yourAge} years old.`);
